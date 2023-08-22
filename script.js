6dd891c0b969c29d642051c6506f334f34a059f0
Sean Keller <sean.keller.sk@gmail.com>
2023-08-22 10:27:23 -0700

    cleaned up project todo comments

--- script.js
+++ script.js
@@ -61,12 +61,6 @@ hourTracker();
 
 }) 
   
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   
 
   
