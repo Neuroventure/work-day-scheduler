fd74c85affa9db225975090f94c80b06787d4e1f
Sean Keller <sean.keller.sk@gmail.com>
2023-08-23 15:53:56 -0700

    rewrote if else if statements to make time blocks show proper colors

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 //wraps the dom to load  html and css first
 $(document).ready(function () {
-//dislays the date in certain format
 
+
+//dislays the date in certain format
 var today = dayjs();
-  $('#displayDay').text(today.format('dddd MMMM D, YYYY'));
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
    //selects text to be added to local storage
   $(".saveBtn").on("click", function () {
    
@@ -22,11 +23,11 @@ $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
 $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-1 .description").val(localStorage.getItem("hour-1"));
-$("#hour-2 .description").val(localStorage.getItem("hour-2"));
-$("#hour-3 .description").val(localStorage.getItem("hour-3"));
-$("#hour-4 .description").val(localStorage.getItem("hour-4"));
-$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
 
@@ -35,26 +36,16 @@ function hourTracker() {
   var currentHour = moment().hour();
 
   // loop over time blocks
-  $(".time-block").each(function () {
-      var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-      console.log( blockHour, currentHour)
-
-      //check if we've moved past this time
-      if (blockHour < currentHour) {
-          $(this).addClass("past");
-          $(this).removeClass("future");
-          $(this).removeClass("present");
-      }
-      else if (blockHour === currentHour) {
-          $(this).removeClass("past");
-          $(this).addClass("present");
-          $(this).removeClass("future");
-      }
-      else {
-          $(this).removeClass("present");
-          $(this).removeClass("past");
-          $(this).addClass("future");
-      }
+  $('.time-block').each(function() {
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
+  
+    if (blockHour < currentHour) {
+      $(this).removeClass('present future').addClass('past');
+    } else if (blockHour === currentHour) {
+      $(this).removeClass('past future').addClass('present');
+    } else {
+      $(this).removeClass('past present').addClass('future');
+    }
   })
 }
 hourTracker();
